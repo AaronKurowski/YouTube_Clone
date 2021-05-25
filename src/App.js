5c94b650f5c7fa21111d90a5121d89cc3d6feefa
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-25 12:24:15 -0500

    edited the iframe with the one provided in the user stories

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to our Youtube Clone!</h1>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/-w-58hQ9dLk"></iframe>    
-    </div>
+      <iframe id="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/embed/-w-58hQ9dLk?autoplay=1&origin=http://example.com"frameborder="0"></iframe>  
+    </div> 
   );
 }
 
