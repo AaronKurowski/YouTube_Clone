7954166af05d5e6ba64a7aa4fdf02ba83ecbe34f
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-25 12:21:29 -0500

    added embedded video

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to our Youtube Clone!</h1>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/-w-58hQ9dLk"></iframe>    
     </div>
   );
 }
