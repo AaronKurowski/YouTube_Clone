038a7a3b505b982a3585777f757a44ec7b6aaf4b
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-27 11:08:59 -0500

    modified videoPlayer.jsx to now have default video and to now display title and description below iframe player

--- src/components/VideoPlayer/videoPlayer.jsx
+++ src/components/VideoPlayer/videoPlayer.jsx
@@ -1,20 +1,38 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import './videoPlayer.css';
 
 const VideoPlayer = (props) => {
+    // This try/catch will handle setting changing the selected video, if the try fails it will render the default video
     try{
-        let url = "https://www.youtube.com/embed/" + props.video.id.videoId// + "?autoplay=1&origin=http://example.com" 
+        let url = "https://www.youtube.com/embed/" + props.video.id.videoId 
         return (
-            <iframe className="vid-frame" id="ytplayer" type="text/html" width="640" height="360" src={url} frameBorder="0"></iframe>
+            <div>
+                <iframe className="vid-frame" id="ytplayer" type="text/html" width="640" height="360" src={url} frameBorder="0"></iframe>
+                <div className="row">
+                    <div className="col-md-1"></div>
+                    <div className="col-md-8">
+                        <h3>{props.video.snippet.title}</h3>
+                        <p>{props.video.snippet.description}</p>
+                    </div>
+                </div>
+            </div>
         );
     }
     catch{
-        let url = "https://www.youtube.com/embed/" + "-w-58hQ9dLk"// + "?autoplay=1&origin=http://example.com" 
+        let url = "https://www.youtube.com/embed/" + "-w-58hQ9dLk" 
         return (
-            <iframe id="ytplayer" type="text/html" width="640" height="360" src={url} frameBorder="0"></iframe>
+            <div>
+                <iframe className="vid-frame" id="ytplayer" type="text/html" width="640" height="360" src={url} frameBorder="0"></iframe>
+                <div className="row">
+                    <div className="col-md-1"></div>
+                    <div className="col-md-8">
+                        <h3>Jurassic Park Theme Song (Melodica Cover)</h3>
+                        <p>Just got a melodica. Here's my rendition of the Jurassic Park theme song. What do you think?</p>
+                    </div>
+                </div>
+            </div>
         );
-    }
-    
+    }   
 }
 
 export default VideoPlayer;
